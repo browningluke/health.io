7b0de743080226d1147d5201b77e415aa1704b57
Luke Browning <lrbrowning6@gmail.com>
2021-02-13 04:47:03 -0500

    Implement Mood and tests.

--- src/main/model/Mood.java
+++ src/main/model/Mood.java
@@ -1,9 +1,75 @@
 package model;
 
+import model.activities.Activity;
+
+import java.util.ArrayList;
+
 public class Mood {
 
-    public Mood() {
+    public static final int MAXMOODSCORE = 5;
 
+    private int moodScore;
+    private ArrayList<Activity> activityList;
+
+
+    public Mood() {
+        moodScore = -1;
+        activityList = new ArrayList<>();
     }
 
+
+    // REQUIRES: activity with same name not already in activityList.
+    // MODIFIES: this
+    // EFFECTS: adds an activity to this mood.
+    public void addActivity(Activity activity) {
+        activityList.add(activity);
+    }
+
+    // MODIFIES: this
+    // EFFECTS: removes an activity with name matching activityName
+    //          from activityList.
+    public void removeActivity(String activityName) {
+        for (Activity a : activityList) {
+            if (a.getActivityName().equals(activityName)) {
+                activityList.remove(a);
+                return;
+            }
+        }
+    }
+
+    // EFFECTS: returns true if an activity with name
+    //          matching activityName is in activityList.
+    public boolean containsActivity(String activityName) {
+        for (Activity a : activityList) {
+            if (a.getActivityName().equals(activityName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // EFFECTS: converts moodScore to a String, unless it is -1
+    //          where it returns "x".
+    public String getUIMoodString() {
+        if (moodScore == -1) {
+            return "x";
+        }
+        return Integer.toString(moodScore);
+    }
+
+    // EFFECTS: returns moodScore value.
+    public int getMoodScore() {
+        return moodScore;
+    }
+
+    // EFFECTS: returns the length of activityList
+    public int getActivityListLength() {
+        return activityList.size();
+    }
+
+    // MODIFIES: this
+    // EFFECTS: returns moodScore value.
+    public void setMoodScore(int ms) {
+        moodScore = ms;
+    }
 }
---
+++ src/test/model/MoodTest.java
@@ -0,0 +1,96 @@
+package model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import model.activities.Activity;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class MoodTest {
+
+    Mood m1;
+    Mood m2;
+
+    @BeforeEach
+    void createMood() {
+        m1 = new Mood();
+        m2 = new Mood();
+    }
+
+    @Test
+    void testInit() {
+        assertEquals(-1, m1.getMoodScore());
+        assertEquals("x", m1.getUIMoodString());
+        assertEquals(0, m1.getActivityListLength());
+
+        assertEquals(-1, m2.getMoodScore());
+        assertEquals("x", m2.getUIMoodString());
+        assertEquals(0, m2.getActivityListLength());
+    }
+
+    @Test
+    void testSetMoodScore() {
+        assertEquals(-1, m1.getMoodScore());
+        assertEquals("x", m1.getUIMoodString());
+        m1.setMoodScore(4);
+        assertEquals(4, m1.getMoodScore());
+        assertEquals("4", m1.getUIMoodString());
+    }
+
+    @Test
+    void testAddActivity() {
+        Activity a1 = new Activity("test1");
+        Activity a2 = new Activity("test2");
+
+        assertEquals(0, m1.getActivityListLength());
+
+        m1.addActivity(a1);
+        assertEquals(1, m1.getActivityListLength());
+        assertTrue(m1.containsActivity("test1"));
+        assertFalse(m1.containsActivity("test2"));
+
+
+        m1.addActivity(a2);
+        assertEquals(2, m1.getActivityListLength());
+        assertTrue(m1.containsActivity("test1"));
+        assertTrue(m1.containsActivity("test2"));
+    }
+
+    @Test
+    void testRemoveActivity() {
+        Activity a1 = new Activity("test1");
+        Activity a2 = new Activity("test2");
+
+        assertEquals(0, m1.getActivityListLength());
+        m1.removeActivity("test");
+        assertEquals(0, m1.getActivityListLength());
+
+        m1.addActivity(a1);
+        assertEquals(1, m1.getActivityListLength());
+        assertTrue(m1.containsActivity("test1"));
+
+        m1.removeActivity("test2");
+        assertEquals(1, m1.getActivityListLength());
+        assertTrue(m1.containsActivity("test1"));
+
+        m1.removeActivity("test1");
+        assertEquals(0, m1.getActivityListLength());
+
+        m1.addActivity(a2);
+        m1.addActivity(a1);
+        assertEquals(2, m1.getActivityListLength());
+        m1.removeActivity("test2");
+        assertEquals(1, m1.getActivityListLength());
+        assertTrue(m1.containsActivity("test1"));
+    }
+
+    @Test
+    void testContainsActivity() {
+        Activity a1 = new Activity("test1");
+        Activity a2 = new Activity("test2");
+        m1.addActivity(a1);
+        assertTrue(m1.containsActivity("test1"));
+        assertFalse(m1.containsActivity("test2"));
+    }
+
+}
